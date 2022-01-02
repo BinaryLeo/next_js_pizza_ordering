bdb6aadb65982b5ac107c5c6469bdc06d50a6e3d
Binaryleo <leomouracss@fastmail.com>
2022-01-02 03:28:21 -0300

    Navbar module.css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,37 @@
+/* eslint-disable react/jsx-no-undef */
+import styles from '../styles/Navbar.module.css'
+import Image from 'next/image'
 const Navbar = () => {
-    return (
-        <div>
-            Navbar Component
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
         </div>
-    )
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW</div>
+          <div className={styles.text}>012 345 678</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="" width="140px" height="49px" />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
